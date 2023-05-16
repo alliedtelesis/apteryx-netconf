8a81ee7fa7fac0b8a5b18894225fe4d874a9328e
Carl Smith <carl.smith@alliedtelesis.co.nz>
2023-05-16 18:16:51 +1200

    test

--- run.sh
+++ run.sh
@@ -146,6 +146,8 @@ rc=$?; if [[ $rc != 0 ]]; then quit $rc; fi
 sleep 0.5
 cd $BUILD/../
 
+netstat -nlap 
+
 if [ $ACTION == "test" ]; then
         echo Running tests ...
         python3 -m pytest -v
